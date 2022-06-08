b9532ddccecf9f0f6bca45f75c27a0b8b4026338
choko0916 <n.yuka0702@gmail.com>
2022-06-09 02:56:52 +0900

    0.1」などの頭に「0」がつく小数点の計算ができるように修正、演算子を押下した際に、小数点が押せないように修正

--- main.js
+++ main.js
@@ -2,37 +2,38 @@
 
 function clickbtn(button) {
   let result = document.getElementById('result');
-  let buttonValue = button.innerHTML;
-  let point = document.getElementById('point');
+  let  buttonValue = button.innerHTML;
+  let Dot = document.getElementById('dot');
   
-    if (buttonValue == "AC") {
+    if ( buttonValue == "AC") {
       result.textContent = 0;
-      point.disabled = false;
+      dot.disabled = false;
     }
-    else if (buttonValue == "=") {
+    else if ( buttonValue == "=") {
       result.textContent = eval(result.textContent);
-      point.disabled = false;
+      dot.disabled = false;
     }
     else {
-      if (result.textContent == 0) {
-        result.textContent = buttonValue;
+      if (result.textContent == "0") {
+        result.textContent =  buttonValue;
+        dot.disabled = false;
       }
       else { 
-        result.textContent = result.textContent + buttonValue;
+        result.textContent = result.textContent +  buttonValue;
+        dot.disabled = false;
       }
     }
   } 
     
   function clickOperator(button) {
     let result = document.getElementById('result');
-    let buttonValue = button.innerHTML;
+    let  buttonValue = button.innerHTML;
     let plus = document.getElementById('plus');
     let minus = document.getElementById('minus');
     let asterisk = document.getElementById('asterisk');
     let slash = document.getElementById('slash');
-    let point = document.getElementById('point');
+    let dot = document.getElementById('dot');
     
-    point.disabled = false;
     
     if (result.textContent.slice(-1) == plus.textContent //もし文字列の最後の文字が+なら
         || result.textContent.slice(-1) == minus.textContent　//または-なら
@@ -41,19 +42,34 @@ function clickbtn(button) {
       {
         let removeLastOperator = result.textContent.slice(0, -1); //文字列の最後の文字を削除
         result.textContent = removeLastOperator + buttonValue;
+        dot.disabled = false;
       }
     else {
-       result.textContent = result.textContent + buttonValue;   
+       result.textContent = result.textContent + buttonValue;
+       dot.disabled = false;
       }
   }  
   
-  function clickPoint(button) {
+  function clickDot(button) {
     let result = document.getElementById('result');
-    let point = document.getElementById('point');
+    let dot = document.getElementById('dot');
     
-    point.disabled = true;
-    result.textContent += point.textContent;
+      if (result.textContent.slice(-1) == ".") {
+       let removeLastDot = result.textContent.slice(0, -1);
+       result.textContent = removeLastDot + "."
+       dot.disabled = false;
+     }
+      else if (result.textContent.slice(-1) == plus.textContent //もし文字列の最後の文字が+なら
+      || result.textContent.slice(-1) == minus.textContent　//または-なら
+      || result.textContent.slice(-1) == asterisk.textContent 
+      || result.textContent.slice(-1) == slash.textContent)
+    {
+      dot.disabled = true; //符号の後は.を押せないようにする
+    }
+      else {
+        result.textContent += dot.textContent;
+        dot.disabled = false;
+      }
   }
   
-//0.7など入力できるようにする→質問する？
 // 1/2 デバッグ勉強する
